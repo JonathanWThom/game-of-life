4c6cebe3327cbfa38587ba8fe49870aa39df5c03
Jonathan Thom <jonathan.thom1990@gmail.com>
2020-03-14 16:26:10 -0700

    README

---
+++ README.md
@@ -0,0 +1,12 @@
+# Conway's Game of Life
+
+Golang CLI implementation of [Conway's Game of Life](https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life).
+
+# Usage
+
+After cloning and `go build`ing, pass the width and height of your cell colony
+to the command, like so: `./game-of-life 10 10`.
+
+# License
+
+MIT
--- cell.go
+++ cell.go
@@ -15,9 +15,8 @@ func (c *Cell) SetLiving() {
 	c.Living = c.NextGenLiving
 }
 
-// can do this concurrently on all
 func (c *Cell) SetNextGenLiving(game *Game) {
-	//   Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+	//  Any live cell with fewer than two live neighbours dies, as if by underpopulation.
 	// Any live cell with two or three live neighbours lives on to the next generation.
 	// Any live cell with more than three live neighbours dies, as if by overpopulation.
 	// Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
